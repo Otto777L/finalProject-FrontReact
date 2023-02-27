0f02f0e0982822e89e4a85ab6f29d69dc1752463
dadiazr07 <dadiazr.07@gmail.com>
2023-02-27 19:06:37 -0400

    Login actualizado con el boton

--- src/js/views/login_register.js
+++ src/js/views/login_register.js
@@ -4,26 +4,26 @@ import React from "react";
 
 export const LogReg = () => (
     <main className="container w-50 m-auto">
-        <form>
-            <img className="mb-4" src="https://i.imgur.com/Hewd1Jo.png" alt="J" style={{ maxHeight: '100px'}}/>
-                <h1 className="h3 mb-3 fw-normal">Inicia sesión aquí</h1>
+        <div className="container justify-content-center">
+            <img className="mb-4" src="https://i.imgur.com/Hewd1Jo.png" alt="J" style={{ maxHeight: '100px' }} />
+            <h1 className="h3 mb-3 fw-normal">Inicia sesión para realizar tus pedidos</h1>
 
-                <div className="form-floating">
-                    <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
-                        <label for="floatingInput">Correo electrónico</label>
-                </div>
-                <div className="form-floating">
-                    <input type="password" className="form-control" id="floatingPassword" placeholder="Password"/>
-                        <label for="floatingPassword">Contraseña</label>
-                </div>
+            <div className="form-floating my-1">
+                <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+                <label for="floatingInput">Correo electrónico</label>
+            </div>
+            <div className="form-floating my-1">
+                <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+                <label for="floatingPassword">Contraseña</label>
+            </div>
+            <button className="learn-more foodButton" type="submit">
+                <span className="circle" aria-hidden="true">
+                    <span className="icon arrow"></span>
+                </span>
+                <span className="button-text">Inicia sesión</span>
+                </button>
+            <p className="mt-5 mb-3 text-muted">© 2023 JUNKIE</p>
 
-                <div className="checkbox mb-3">
-                    <label>
-                        <input type="checkbox" value="remember-me"/> Recuérdame
-                    </label>
-                </div>
-                <button className="w-100 btn btn-lg btn-danger" type="submit">Inicia sesión</button>
-                <p className="mt-5 mb-3 text-muted">© 2023 JUNKIE</p>
-        </form>
+        </div>
     </main>
 )

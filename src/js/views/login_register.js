3701d693e63eb2602329d009b1044f49a8c822c9
dadiazr07 <dadiazr.07@gmail.com>
2023-03-03 19:16:45 -0400

    Funcionalidad login parcial

--- src/js/views/login_register.js
+++ src/js/views/login_register.js
@@ -8,6 +8,44 @@ export function LogReg() {
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
     const [select, setSelect] = useState("")
+    const [passwordShown, setPasswordShown] = useState(false);
+
+
+    const [userList, setUserList] = useState([])
+
+
+    const users = [
+        {
+            username: 'dadiazr.07@gmail.com',
+            password: 'admin1@1',
+        },
+        {
+            username: 'admin2@gmail.com',
+            password: 'admin2@2',
+        },
+    ];
+
+
+    const handleSubmit = (e) => {
+        console.log(users)
+        e.preventDefault();
+
+        const toCompare = {
+            username: e.target.elements.username.value,
+            password: e.target.elements.password.value,
+        };
+
+        if (users.some((u) => JSON.stringify(u) === JSON.stringify(toCompare))) {
+            console.log('User exists in array');
+        } else {
+            console.log('User does not exist in array');
+        }
+    };
+
+
+    const togglePassword = () => {
+        setPasswordShown(!passwordShown);
+      };
 
 
     return (
@@ -21,20 +59,25 @@ export function LogReg() {
                         <img className="mb-4 img-fluid" src="https://i.imgur.com/Hewd1Jo.png" alt="J" style={{ height: '100px', aspectRatio: 1 / 1 }} />
                         <h3 className="h3 mb-3 fw-normal">Inicia sesión para realizar tus pedidos</h3>
 
-                        <div className="form-floating w-75 my-1">
-                            <input type="email" className="form-control" placeholder="name@example.com" />
-                            <label htmlFor="floatingInput">Correo electrónico</label>
-                        </div>
-                        <div className="form-floating w-75 my-1">
-                            <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
-                            <label htmlFor="floatingPassword">Contraseña</label>
-                        </div>
-                        <button className="shopButton">Iniciar sesión</button>
+                        <form className="w-100" onSubmit={handleSubmit}>
+                            <div className="form-floating w-100 my-1">
+                                <input name="username" type="email" className="form-control" placeholder="name@example.com" />
+                                <label htmlFor="floatingInput">Correo electrónico</label>
+                            </div>
+                            <div className="d-flex align-items-baseline">
+                                <div className="form-floating w-75 my-1">
+                                    <input name="password" type={passwordShown ? "text" : "password"} className="form-control" id="floatingPassword" placeholder="Password" />
+                                    <label className="w-75" htmlFor="floatingPassword">Contraseña</label>
+                                </div>
+                                <label className="w-25 link-danger btn fw-bold" onClick={togglePassword}>OJO</label>
+                            </div>
+                            <button className="shopButton" type="submit">Iniciar sesión</button>
+                        </form>
+
                         <div className="d-flex align-items-baseline">
                             <p>¿No tienes cuenta?</p>
-                            <span class="link-danger btn fw-bold" onClick={() => { setSelect("register") }}>¡Regístrate!</span>
+                            <span className="link-danger btn fw-bold" onClick={() => { setSelect("register") }}>¡Regístrate!</span>
                         </div>
-
 
                         <p className="mt-5 mb-3 text-muted">© 2023 JUNKIE</p>
 
@@ -60,7 +103,7 @@ export function LogReg() {
                     <button className="shopButton">Regístrate</button>
                     <div className="d-flex align-items-baseline">
                         <p>¿Ya tienes cuenta?</p>
-                        <span class="link-danger btn fw-bold" onClick={() => { setSelect("login") }}>¡Inicia sesión!</span>
+                        <span className="link-danger btn fw-bold" onClick={() => { setSelect("login") }}>¡Inicia sesión!</span>
                     </div>
 
                     <p className="mt-5 mb-3 text-muted">© 2023 JUNKIE</p>
@@ -74,69 +117,3 @@ export function LogReg() {
         </>
     )
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-{/* <div className="container w-100 m-auto">
-                <div className="d-flex flex-column justify-content-center align-items-center">
-                    <img className="mb-4 img-fluid" src="https://i.imgur.com/Hewd1Jo.png" alt="J" style={{ height: '100px', aspectRatio: 1 / 1 }} />
-                    <h3 className="h3 mb-3 fw-normal">Regístrate para realizar tus pedidos</h3>
-
-                    <div className="w-75 form-floating my-1">
-                        <input type="nombre" className="form-control" placeholder="Nombre" />
-                        <label htmlFor="floatingInput">Nombre</label>
-                    </div>
-                    <div className="w-75 form-floating my-1">
-                        <input type="apellido" className="form-control" placeholder="Apellido" />
-                        <label htmlFor="floatingInput">Apellido</label>
-                    </div>
-
-                    <div class="d-md-flex justify-content-start align-items-center mb-1 py-2">
-                        <h6 class="mb-0 me-4">Género: </h6>
-                        <div class="form-check form-check-inline mb-0 me-4">
-                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="femaleGender"
-                                value="option1" />
-                            <label class="form-check-label" for="femaleGender">Mujer</label>
-                        </div>
-                        <div class="form-check form-check-inline mb-0 me-4">
-                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="maleGender"
-                                value="option2" />
-                            <label class="form-check-label" for="maleGender">Hombre</label>
-                        </div>
-                        <div class="form-check form-check-inline mb-0">
-                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="otherGender"
-                                value="option3" />
-                            <label class="form-check-label" for="otherGender">Otro</label>
-                        </div>
-                    </div>
-                    <div className="w-75 form-floating my-1">
-                        <input type="tel" className="form-control" placeholder="Teléfono" />
-                        <label htmlFor="floatingInput">Teléfono</label>
-                    </div>
-                    <div className="w-75 form-floating my-1">
-                        <input type="dirección" className="form-control" placeholder="Dirección" />
-                        <label htmlFor="floatingInput">Dirección</label>
-                    </div>
-                    <button className="shopButton">Regístrate</button>
-
-                    <p className="mt-5 mb-3 text-muted">© 2023 JUNKIE</p>
-                </div>
-            </div> */}

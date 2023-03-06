ba423f1212f1af1927e3bb97b3cc49e17ef83beb
Otto777L <otto.lh12@gmail.com>
2023-03-06 22:14:35 +0000

    added map api to shopping cart

--- src/js/component/CardCatalog.js
+++ src/js/component/CardCatalog.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import "../../styles/card-catalog.css";
 
 export const CardCatalog = ({item}) => {    
   
     return (
         <div className="card m-2" style={{width: '250px'}}>
-        <img src={item.img} className="card-img-top" alt="..."></img>
+        <img src={item.img} className="card-img-top product-img" alt="..."></img>
         <div className="card-body d-flex justify-content-between">
           <div className="">
             <h5 className="card-title">{item.name}</h5>
